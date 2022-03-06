e996324daf530d9005269ba1679c3bb07aaec4de
Venky Iyer <indigoviolet@gmail.com>
2022-03-06 10:12:34 -0800

    fixes

--- src/nbmetadata/app.py
+++ src/nbmetadata/app.py
@@ -2,17 +2,15 @@ from __future__ import annotations
 
 import json
 from pathlib import Path
-from typing import Optional, List
+from typing import Optional, List, Dict
 import _jsonnet as jsonnet
 import typer
 from importlib_resources import files
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import re
 from functools import cached_property
-import fileinput
 import nbformat
-from deepmerge import Merger
-import sys
+from deepmerge import Merger as DeepMerger
 from in_place import InPlace
 import icecream
 
@@ -45,15 +43,35 @@ class Config:
         return self.merged[key]
 
 
-def make_merger() -> Merger:
-    return Merger(
-        [(dict, ["merge"]), (list, [append_list_unique])], ["override"], ["override"]
-    )
-
-
 def append_list_unique(config, path, base: List, nxt: List):
     base.extend([k for k in nxt if k not in base])
     return base
+
+
+@dataclass
+class Merger:
+    deep_merger: DeepMerger = field(init=False)
+    journal_key: str = "_nbmd_keys"
+
+    def __post_init__(self):
+        self.deep_merger = DeepMerger(
+            [(dict, ["merge"]), (list, [append_list_unique])],
+            ["override"],
+            ["override"],
+        )
+
+    def merge(self, node: nbformat.notebooknode.NotebookNode, data: Dict):
+        data.update({self.journal_key: list(data.keys())})
+        self.deep_merger.merge(node, data)
+
+    def clean(self, node: nbformat.notebooknode.NotebookNode) -> bool:
+        if self.journal_key not in node:
+            return False
+        keys_to_clean = node.pop(self.journal_key)
+        for k in keys_to_clean:
+            if k in node:
+                del node[k]
+        return True
 
 
 def add_metadata(
@@ -69,7 +87,7 @@ def add_metadata(
         del to_add["_top"]
         merger.merge(notebook.metadata, to_add)
     else:
-        merger.merge(cell, to_add)
+        merger.merge(cell.metadata, to_add)
 
 
 @run.command()
@@ -80,18 +98,23 @@ def main(
 ):
     cfg = Config(user_config_file=config_file)
     comment_re = re.compile(rf"^\s*#+\s*{prefix}(?P<key>\w+)", re.MULTILINE)
-    merger = make_merger()
+    merger = Merger()
 
     with InPlace(name=notebook_file, backup_ext=".bak") as nf:
         modified = False
+
         notebook = nbformat.read(nf, as_version=nbformat.NO_CONVERT)
+        modified |= merger.clean(notebook.metadata)
         for cell in notebook.cells:
+            modified |= merger.clean(cell.metadata)
             keys = [m.group("key") for m in comment_re.finditer(cell.source)]
             for k in set(keys):
                 add_metadata(k, cell, notebook, config=cfg, merger=merger)
                 modified = True
         if modified:
             nbformat.write(notebook, nf)
+        else:
+            nf.rollback()
 
 
 if __name__ == "__main__":
--- src/nbmetadata/config.jsonnet
+++ src/nbmetadata/config.jsonnet
@@ -2,13 +2,13 @@ function(user_config={})
   local base_config = {
     nbmake_timeout_300: {
       _top: true,
-      metadata: { execution: { timeout: 300 } },
+      execution: { timeout: 300 },
     },
     nbmake_raises: {
-      custom: { metadata: { tags: ['raises-exception'] } },
+      tags: ['raises-exception'],
     },
     nbmake_skip: {
-      custom: { metadata: { tags: ['skip-execution'] } },
+      tags: ['skip-execution'],
     },
   };
   base_config + user_config
